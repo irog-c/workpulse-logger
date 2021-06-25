bc109ef225343f7d4cd34e2e2fcb5a1b721f2bf4
irog-c <mihajlov.igor15@gmail.com>
2021-06-25 18:25:22 +0200

    Initial commit

---
+++ FileWatcher/FileWatcher/FileWatcher/Watcher.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace FileWatcher
+{
+    class Watcher
+    {
+        private const string fileToWatch = "C:\\log-app.txt";
+        private const string dirToWatch = "C:";
+        private const string logFile = "C:\\log-open-log-file.txt";
+
+        static FileSystemWatcher watcher;
+
+        private static string getCurrentUserName()
+        {
+            return System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+        }
+        
+        private static void LogEvent(String ev)
+        {
+            string user = getCurrentUserName();
+            File.AppendAllText(logFile, user + ": " + ev + "\n");
+        }
+
+        private static void WatcherEventCallback(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.FullPath);
+            if(e.FullPath == fileToWatch)
+                LogEvent("File " + e.Name + " has been opened.");
+        }
+
+        private static void InitWatcher()
+        {
+            watcher = new FileSystemWatcher();
+
+            watcher.Path = dirToWatch;
+            watcher.NotifyFilter = NotifyFilters.LastAccess;
+            watcher.Filter = Path.GetFileName(fileToWatch);
+
+            watcher.Changed += WatcherEventCallback;
+            watcher.EnableRaisingEvents = true;
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            InitWatcher();
+            System.Windows.Forms.Application.Run(new ApplicationContext());
+        }
+    }
+}
---
+++ LogService/LogService/LogService/Log.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogService
+{
+    public partial class Log : ServiceBase
+    {
+        private static string fileWatcher = "F:\\VisualStudioRepos\\FileWatcher\\FileWatcher\\FileWatcher\\bin\\Debug\\FileWatcher.exe";
+        private static string loggerApplication = "F:\\VisualStudioRepos\\TestProject\\TestProject\\Debug\\TestProject.exe";
+
+        public Log()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            ProcessExtensions.StartProcessAsCurrentUser(fileWatcher);
+            ProcessExtensions.StartProcessAsCurrentUser(loggerApplication);
+        }
+
+        private static void KillAll(string path)
+        {
+            string name = Path.GetFileNameWithoutExtension(path);
+            Process[] workers = Process.GetProcessesByName(name);
+            foreach (Process worker in workers)
+            {
+                worker.Kill();
+                worker.WaitForExit();
+                worker.Dispose();
+            }
+        }
+
+        protected override void OnStop()
+        {
+            KillAll(fileWatcher);
+            KillAll(loggerApplication);
+        }
+    }
+}
